c7034256f77e4896cd8798db18204c9e9fe6223c
Frshaad <76130454+frshaad@users.noreply.github.com>
2022-06-01 23:29:18 +0430

    Minor changes to js

--- fem-10-Article-preview-component/src/js/index.js
+++ fem-10-Article-preview-component/src/js/index.js
@@ -1,8 +1,22 @@
 const shareBtn = document.querySelector('.content__author__btn');
+const shareBtnSvg = document.querySelector('.content__author__btn--svg');
 const desktopToolTip = document.querySelector('.content__author__shared--desktop');
 const mobileToolTip = document.querySelector('.content__author__shared--mobile');
 
-shareBtn.addEventListener('click', () => {
-	desktopToolTip.style.display = 'flex';
-	mobileToolTip.style.display = 'flex';
+// shareBtn.addEventListener('click', () => {
+// 	desktopToolTip.style.display = 'flex';
+// 	mobileToolTip.style.display = 'flex';
+// });
+
+document.body.addEventListener('click', e => {
+	if (
+		e.target.classList.contains('content__author__btn') ||
+		e.target.classList.contains('content__author__btn--svg')
+	) {
+		desktopToolTip.style.display = 'flex';
+		mobileToolTip.style.display = 'flex';
+	} else {
+		desktopToolTip.style.display = 'none';
+		mobileToolTip.style.display = 'none';
+	}
 });
