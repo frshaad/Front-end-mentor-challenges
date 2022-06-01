90bda184917b2388ddf7283a30700a85b672c0f1
Frshaad <76130454+frshaad@users.noreply.github.com>
2022-06-01 20:12:20 +0430

    projecet 10 almost done

--- fem-10-Article-preview-component/src/js/index.js
+++ fem-10-Article-preview-component/src/js/index.js
@@ -1,1 +1,8 @@
-console.log(123);
+const shareBtn = document.querySelector('.content__author__btn');
+const desktopToolTip = document.querySelector('.content__author__shared--desktop');
+const mobileToolTip = document.querySelector('.content__author__shared--mobile');
+
+shareBtn.addEventListener('click', () => {
+	desktopToolTip.style.display = 'flex';
+	mobileToolTip.style.display = 'flex';
+});
