3fbd4e65fd5e04b3414d31ac69931bbcf2158766
Frshaad <76130454+frshaad@users.noreply.github.com>
2022-06-02 21:15:00 +0430

    fem11 js almost done

--- fem-11-Intro-component-with-sign-up-form/src/js/index.js
+++ fem-11-Intro-component-with-sign-up-form/src/js/index.js
@@ -1,11 +1,40 @@
 const submitBtn = document.querySelector('.interactive__signup__submit');
 const inputs = document.querySelectorAll('input');
+const fNameInput = document.getElementById('fName');
+const fNameErrors = document.querySelectorAll('.fname-error');
+const lNameInput = document.getElementById('lName');
+const lNameErrors = document.querySelectorAll('.lname-error');
+const emailInput = document.getElementById('email');
+const emailErrors = document.querySelectorAll('.email-error');
+const passwordInput = document.getElementById('password');
+const passwordErrors = document.querySelectorAll('.pass-error');
 
 submitBtn.addEventListener('click', () => {
-	inputs.forEach(input => {
-		console.log(input.value);
-		if (input.value === '' && input.hasAttribute('required')) {
-			console.log('ok');
-		}
-	});
+	if (fNameInput.value === '') {
+		fNameErrors.forEach(fnameError => {
+			fnameError.style.display = 'block';
+			fNameInput.style.border = '3px solid hsl(0, 100%, 74%)';
+		});
+	}
+
+	if (lNameInput.value === '') {
+		lNameErrors.forEach(lnameError => {
+			lnameError.style.display = 'block';
+			lNameInput.style.border = '3px solid hsl(0, 100%, 74%)';
+		});
+	}
+
+	if (emailInput.value === '' || emailInput.validity.typeMismatch) {
+		emailErrors.forEach(emailError => {
+			emailError.style.display = 'block';
+			emailInput.style.border = '3px solid hsl(0, 100%, 74%)';
+		});
+	}
+
+	if (passwordInput.value === '') {
+		passwordErrors.forEach(passwordError => {
+			passwordError.style.display = 'block';
+			passwordInput.style.border = '3px solid hsl(0, 100%, 74%)';
+		});
+	}
 });
