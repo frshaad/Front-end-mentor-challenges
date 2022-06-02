101c9b09287646c8372276e9d6df20788ec2d6be
Frshaad <76130454+frshaad@users.noreply.github.com>
2022-06-02 18:22:16 +0430

    project 11 html and css complete

--- fem-11-Intro-component-with-sign-up-form/src/js/index.js
+++ fem-11-Intro-component-with-sign-up-form/src/js/index.js
@@ -1,1 +1,11 @@
-console.log(123);
+const submitBtn = document.querySelector('.interactive__signup__submit');
+const inputs = document.querySelectorAll('input');
+
+submitBtn.addEventListener('click', () => {
+	inputs.forEach(input => {
+		console.log(input.value);
+		if (input.value === '' && input.hasAttribute('required')) {
+			console.log('ok');
+		}
+	});
+});
