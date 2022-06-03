4fcf455e860a9507a86b59343191409666e84e78
Frshaad <76130454+frshaad@users.noreply.github.com>
2022-06-03 22:24:40 +0430

    fem12 done

--- fem-12-Ping-single-column-coming-soon-page/src/js/index.js
+++ fem-12-Ping-single-column-coming-soon-page/src/js/index.js
@@ -1,1 +1,13 @@
-console.log(123);
+const submitBtn = document.querySelector('.btn');
+const emailInput = document.getElementById('email');
+const errorMessage = document.querySelector('.error');
+
+console.log(emailInput.validity);
+
+submitBtn.addEventListener('click', event => {
+	if (emailInput.value === '' || emailInput.validity.typeMismatch) {
+		emailInput.style.border = '2px solid hsl(354, 100%, 66%)';
+		errorMessage.style.display = 'block';
+		event.preventDefault();
+	}
+});
