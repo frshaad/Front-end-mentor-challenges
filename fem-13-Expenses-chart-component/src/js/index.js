41f1007e586db40b16dcff958de98fa8c1da4757
Frshaad <76130454+frshaad@users.noreply.github.com>
2022-06-04 18:41:43 +0430

    fem13 done

--- fem-13-Expenses-chart-component/src/js/index.js
+++ fem-13-Expenses-chart-component/src/js/index.js
@@ -1,1 +1,25 @@
-console.log(123);
+const dataBars = document.querySelectorAll('.chart__bar__datas__data');
+const spends = document.querySelectorAll('.spend');
+
+fetch('../data.json')
+	.then(data => data.json())
+	.then(arr => {
+		let sortArr = [];
+		for (let j = 0; j < arr.length; j++) {
+			sortArr.push(arr[j].amount);
+		}
+		let max = Math.max.apply(null, sortArr);
+		let maxIndex = sortArr.indexOf(max);
+
+		for (let i = 0; i < arr.length; i++) {
+			let height = Math.round((arr[i].amount / max) * 140);
+			dataBars[i].style.height = `${height}px`;
+			spends[i].innerHTML = `$${arr[i].amount}`;
+		}
+		dataBars[maxIndex].classList.add('max-spend');
+		spends.forEach(spend => {
+			if (spend.innerHTML === '') {
+				spend.style.display = 'block';
+			}
+		});
+	});
