8d27ee98ccffa380c72232c60769ea42505d7e5d
Frshaad <76130454+frshaad@users.noreply.github.com>
2022-06-06 16:31:39 +0430

    fem15 js almost done

--- fem-15-Advice-generator-app/src/js/index.js
+++ fem-15-Advice-generator-app/src/js/index.js
@@ -1,1 +1,14 @@
-console.log(123);
+const url = 'https://api.adviceslip.com/advice';
+const diceBtn = document.querySelector('.dice-container');
+const adviceId = document.querySelector('.advice-id');
+const quote = document.querySelector('.quote');
+
+diceBtn.addEventListener('click', () => {
+	fetch(url, { method: 'GET' })
+		.then(response => response.json())
+		.then(data => {
+			adviceId.innerHTML = data.slip.id;
+			quote.innerHTML = data.slip.advice;
+		})
+		.catch(error => console.log(error));
+});
